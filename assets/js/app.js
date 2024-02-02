c05106f3f155f5bc1ce271b3fb8923ad0249552b
Dorian NUNEZ <dorian@wecrea.com>
2024-02-02 14:20:14 +0100

    Frond profile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
-(() => {
-    console.log('Hello World');
+document.addEventListener('DOMContentLoaded', function () {
+    function openClose(elementId) {
+        let element = document.getElementById(elementId);
+
+        // Si l'élément est actuellement caché, l'ouvrir
+        if (element.style.display === 'none' || element.style.display === '') {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none'; // Sinon, fermer l'élément
+        }
+    }
+
+
+    document.getElementById('button-dropdown-profile').onclick = () => {
+        openClose('dropdown-profile')
+    }
 })();
--- templates/main.php
+++ templates/main.php
@@ -5,12 +5,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- Chargement des ressources -->
-        <script src="assets/js/app.js"></script>
+     
 		<script src="https://cdn.tailwindcss.com"></script>
-
+		<script src="../assets/js/app.js"></script>
 		<title> <?= $data['title'] ?> </title>
 	</head>
 	<body>
+		<?php if(isset($_SESSION["login"])){ 
+		 include_once __DIR__."/_header.php";  
+		 include_once __DIR__."/_sidebar.php";
+		}?>
 		<main>
 			<?php require_once $templatePath ?>
 		</main>

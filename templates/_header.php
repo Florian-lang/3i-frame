554d43b72b7bca03d40c2b7d895b542b0c74caea
Dorian NUNEZ <dorian@wecrea.com>
2024-02-02 14:20:14 +0100

    Frond profile

--- templates/_header.php
+++ templates/_header.php
@@ -16,15 +16,15 @@
       <div class="flex items-center">
 
           <!-- Profile -->
-          <div class="flex items-center ml-3">
-            <div>
-              <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button-2" aria-expanded="false" data-dropdown-toggle="dropdown-2">
+          <div class="flex flex-col items-center ml-3 static">
+            <div id="button-dropdown-profile">
+              <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"  aria-expanded="false" data-dropdown-toggle="dropdown-2">
                 <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo" id="button-dropdown-profile">
               </button>
             </div>
             <!-- Dropdown menu -->
-            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-2">
+            <div class="absolute right-0 mt-10 z-10 hidden  text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-profile">
               <div class="px-4 py-3" role="none">
                 <p class="text-sm text-gray-900 dark:text-white" role="none">
                   Neil Sims
--- templates/main/home.php
+++ templates/main/home.php
@@ -1,6 +1,4 @@
 <?php use iFrame\Router\Router;?>
-<?php include_once __DIR__."/../_header.php";  ?>
-<?php include_once __DIR__."/../_sidebar.php";  ?>
 
 <h1 class="text-3xl font-bold underline"> <?= $data['content'] ?> </h1>
 <a href="<?=  Router::generate("aaaa") ?>">Cliquer</a>

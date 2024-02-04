bd5e9e8d0725d2c610b7e32c2d369b9af7797964
Dorian NUNEZ <dorian@wecrea.com>
2024-02-04 18:09:40 +0100

    Update User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,13 @@ class User
     private int $id;
     private string $email;
     private string $password;
-    private string $name;
+    private string $firstname;
+    private string $lastname;
+    private ?string $address;
+    private ?string $country;
+    private ?string $city;
+    private ?string $postal_code;
+    private ?string $phone;
     private ?string $image;
 
     public function getId(): int
@@ -45,15 +51,82 @@ class User
         return $this;
     }
 
-    public function getName(): string
+    public function getFirstName(): string
     {
-        return $this->name;
+        return $this->firstname;
     }
 
-    public function setName(string $name): self
+    public function setFirstName(string $firstname): self
     {
-        $this->name = $name;
+        $this->firstname = $firstname;
 
+        return $this;
+    }
+
+    public function getLasttName(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastName(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): self
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(?string $postal_code): self
+    {
+        $this->postal_code = $postal_code;
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
         return $this;
     }
 
@@ -65,7 +138,6 @@ class User
     public function setImage(?string $image): self
     {
         $this->image = $image;
-        var_dump($image);
         return $this;
     }
 }
--- templates/auth/register.php
+++ templates/auth/register.php
@@ -26,6 +26,16 @@ use iFrame\Router\Router;
                 S'inscrire
             </h2>
             <form class="mt-8 space-y-6" action="<?= Router::generate("app_register") ?>" method="POST">
+                <div class="flex justify-between gap-8">
+                    <div class="w-full">
+                    <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+                    <input type="text" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nunez" required>
+                    </div>
+                    <div class="w-full">
+                    <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prénom</label>
+                    <input type="text" name="firstname" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Dorian" required>
+                    </div>
+                </div>
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                     <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="name@company.com" required>

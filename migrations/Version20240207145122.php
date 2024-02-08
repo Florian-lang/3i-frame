754f59f5fc8603967edc1e131bd135fc98667424
Dorian NUNEZ <dorian@wecrea.com>
2024-02-08 11:53:34 +0100

    Improve sql call

--- migrations/Version20240207145122.php
+++ migrations/Version20240207145122.php
@@ -11,25 +11,18 @@ final class Version20240207145122 extends AbstractMigration {
 
     public function up() : void
     {
-        $query = "CREATE TABLE \"product\" (
+        $this->addSql("CREATE TABLE \"product\" (
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             description VARCHAR(255) NOT NULL,
             price VARCHAR(255),
             image VARCHAR(255),
             category_id INT
-            );";
-
-            $requete = $this->em->getConnexion()->prepare($query);
-            $requete->execute();
-
+            );");
     }
 
     public function down() : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $query = "DROP TABLE IF EXIST \"product\";";
-        $requete = $this->em->getConnexion()->prepare($query);
-        $requete->execute();
+        $this->addSql("DROP TABLE IF EXIST \"product\";");
     }
 }
